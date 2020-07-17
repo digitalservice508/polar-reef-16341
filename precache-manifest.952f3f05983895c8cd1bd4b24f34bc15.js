de2dff80bba1451e264a9f057fed2eff04cecf0f
digitalservice508 <68083745+digitalservice508@users.noreply.github.com>
2020-07-17 23:32:03 +0500

    Add files via upload

---
+++ precache-manifest.952f3f05983895c8cd1bd4b24f34bc15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e024315eae1c4355d46152e2868d15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be9941ab1fabe870767",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "951abf87cbe2bd01a884",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6be9941ab1fabe870767",
+    "url": "/static/js/2.8c810931.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c810931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951abf87cbe2bd01a884",
+    "url": "/static/js/main.bd5d1736.chunk.js"
+  },
+  {
+    "revision": "84bded8fd266a73c0da1",
+    "url": "/static/js/runtime-main.dc8d5faa.js"
+  }
+]);
